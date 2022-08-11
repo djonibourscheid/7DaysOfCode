d65becd07e7a5a673f11df4ddc57418a56eb6a58
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-08-11 11:08:57 -0300

    Inicio do conceito

---
+++ Lógica com JavaScript/dia07.js
@@ -0,0 +1,11 @@
+soma(2 + "2")
+
+function soma(valor1, valor2) {
+  const resultado = Number(valor1) + Number(valor2);
+
+  // if(isNaN(resultado)) {
+  //   return console.log("Valores inválidos");
+  // } else {
+  //   return resultado;
+  // }
+}
