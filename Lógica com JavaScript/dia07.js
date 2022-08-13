8b2d4c0eac8d09d8ad62e3dbe71f191c456561c0
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-08-12 23:24:28 -0300

    Código js básico

--- Lógica com JavaScript/dia07.js
+++ Lógica com JavaScript/dia07.js
@@ -1,11 +1,30 @@
-soma(2 + "2")
+console.log("Use soma(valor1, valor2) para fazer adições, subtracao(..), multiplicacao() e divisao()");
 
 function soma(valor1, valor2) {
   const resultado = Number(valor1) + Number(valor2);
 
-  // if(isNaN(resultado)) {
-  //   return console.log("Valores inválidos");
-  // } else {
-  //   return resultado;
-  // }
+  validar(resultado)
+}
+function subtracao(valor1, valor2) {
+  const resultado = Number(valor1) - Number(valor2);
+
+  validar(resultado)
+}
+function multiplicacao(valor1, valor2) {
+  const resultado = Number(valor1) * Number(valor2);
+
+  validar(resultado)
+}
+function divisao(valor1, valor2) {
+  const resultado = Number(valor1) / Number(valor2);
+
+  validar(resultado)
+}
+
+function validar(resultado) {
+  if(isNaN(resultado)) {
+    return console.log("Valores inválidos");
+  } else {
+    return console.log(resultado);
+  }
 }
