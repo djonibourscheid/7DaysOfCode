9c5e86846fe492063aa1c2691fe15ac81a8a0106
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-08-13 23:18:54 -0300

    Criando loop e verificação

--- Lógica com JavaScript/dia07.js
+++ Lógica com JavaScript/dia07.js
@@ -1,6 +1,6 @@
-console.log("Use soma(valor1, valor2) para fazer adições, subtracao(..), multiplicacao() e divisao()");
+let operacao, valor1, valor2;
 
-function soma(valor1, valor2) {
+function adicao(valor1, valor2) {
   const resultado = Number(valor1) + Number(valor2);
 
   validar(resultado)
@@ -22,9 +22,45 @@ function divisao(valor1, valor2) {
 }
 
 function validar(resultado) {
-  if(isNaN(resultado)) {
-    return console.log("Valores inválidos");
+  if (isNaN(resultado)) {
+    return alert("Valores inválidos");
   } else {
-    return console.log(resultado);
+    return alert(`O resultado é ${resultado}`);
   }
 }
+
+do {
+  let continuar = true;
+  operacao = prompt("Qual operação você deseja realizar? 'Adicão' 'Subtração' 'multiplicação' ou 'divisão'").toLowerCase().trim();
+  console.log(operacao);
+  if (operacao === "adição" || operacao === "subtração" || operacao === "multiplicação" || operacao === "divisão") {
+    valor1 = Number(prompt("Qual primeiro valor?").trim());
+    valor2 = Number(prompt("Qual segundo valor?").trim());
+  }
+
+  switch (operacao) {
+    case 'adição':
+      adicao(valor1, valor2);
+      break;
+    case 'subtração':
+      subtracao(valor1, valor2);
+      break;
+    case 'multiplicação':
+      multiplicacao(valor1, valor2);
+      break;
+    case 'divisão':
+      divisao(valor1, valor2);
+      break;
+    case 'sair':
+      alert("Volte sempre!");
+      continuar = false;
+      break;
+    default:
+      if (operacao != null) {
+        alert("Operação inválida.")
+      } else {
+        continuar = false;
+      };
+      break;
+  }
+} while (continuar);
