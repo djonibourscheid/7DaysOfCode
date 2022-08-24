774c95c61d49e80a890772f79734ffc45584db69
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-08-24 03:21:12 -0300

    Script refatorado para integração com html
    deu trabalin'-'

--- LogicWithJS/exercise04/script.js
+++ LogicWithJS/exercise04/script.js
@@ -1,29 +1,117 @@
-const numberMax = 10;
-const numberMin = 0;
+let chances, spokenNumbers, numberSelected;
 
-function comecar() {
-  const numberSelected = Math.floor(Math.random() * (numberMax - numberMin + 1) + numberMin);
-  let chances = 3;
+function setupGame() {
+  chances = 3;
+  spokenNumbers = [];
 
-  do {
-    chances--;
-    const numberInput = Math.floor(prompt(`Digite o número de ${numberMin} a ${numberMax}, que você acha que é: `));
-    // Se o usuário digitar um número quebrado (12.34) vai ser 12 por conta do .floor()
-    // Se o usuário digitar uma letra vai dar NaN e o if com isNaN vai dar true e vai dar mais uma chance
+  // Valores mínimo e máximo
+  let numberMin = document.getElementById("minNum").value;
+  let numberMax = document.getElementById("maxNum").value;
 
-    if (numberInput < numberMin || numberInput > numberMax || isNaN(numberInput)) {
-      alert(`Digite um número válido (${numberMin} a ${numberMax})!`);
-      chances++;
+  if (numberMin === "" || numberMin <= 0) { numberMin = 1; }
+  if (numberMax === "") { numberMax = 10; }
 
-    } else if (numberInput === numberSelected) {
-      return alert(`Você acertou, o número selecionado era ${numberSelected}!`);
+  const verification = verificationMinSmallMax(numberMin, numberMax);
+  // Se passar na verificação retorna true
+  if (verification) {
+    // Mostrando o lugar onde o usuário irá inserir os números
+    const inputNumberSection = document.querySelector(".section.inputNumber");
+    inputNumberSection.classList.remove("hide");
 
-    } else if (numberInput !== numberSelected) {
-      if (chances === 0) {
-        alert(`Você errou e suas tentativas acabaram. Mais sorte na próxima.`);
-        return alert(`O número selecionado era ${numberSelected}`);
-      }
-      alert(`Você errou. Tente novamente. Restam ${chances} tentativas`);
+    // Botando o placeholder no input do número mínimo máximo
+    const inputNumberEl = document.getElementById("inputNumberEl");
+    inputNumberEl.placeholder = `${numberMin} a ${numberMax}`;
+
+    // Configurando o botão de enviar o número do jogador com o numero min e max
+    const buttonInputNumber = document.getElementById("buttonInputNumber");
+    buttonInputNumber.setAttribute("onclick", `game(${numberMin}, ${numberMax})`);
+    buttonInputNumber.disabled = false;
+
+    // Escondendo a área de mensagens
+    const messageAlertEl = document.querySelector(".messageAlert");
+    messageAlertEl.classList.add("hide");
+    messageAlertEl.innerText = "";
+
+    // Limpa a área de respostas
+    const spokenNumbersEl = document.querySelector(".numbers");
+    spokenNumbersEl.innerHTML = "";
+
+    // Gera o número que o usuário deverá acertar
+    numberSelected = Math.floor(Math.random() * (numberMax - numberMin + 1) + numberMin);
+  }
+}
+
+function verificationMinSmallMax(numberMin, numberMax) {
+  if (numberMin > numberMax) {
+    alert("Número mínimo é maior ou igual ao número máximo!");
+    return false;
+  } else return true;
+}
+
+function game(numberMin, numberMax) {
+  chances--;
+  const inputNumberEl = document.getElementById("inputNumberEl");
+
+  const numberInput = Math.floor(inputNumberEl.value);
+  inputNumberEl.value = "";
+
+  /*
+    Se o usuário digitar um número quebrado (12.34) vai ser 12 por conta do .floor()
+    Se o usuário digitar duas vezes o mesmo número, dá uma chance na segunda digitada
+  */
+
+  if (numberInput < numberMin || numberInput > numberMax) {
+    messageAlert(`Digite um número válido (${numberMin} a ${numberMax})!`);
+    return chances++;
+  } else if (spokenNumbers.indexOf(numberInput) !== -1) {
+    messageAlert("Digite um número que ainda não tenha digitado!");
+    return chances++;
+  }
+
+  spokenNumbers.push(numberInput); // adiciona o número nos números já ditos
+
+  if (numberInput === numberSelected) {
+    addSpokenNumbersScreen(numberInput, true); // adiciona o número na tela
+
+    // desabilita o usuario de continuar jogando sem iniciar um novo setup
+    const buttonInputNumber = document.getElementById("buttonInputNumber");
+    buttonInputNumber.disabled = true;
+
+    messageAlert(`Você acertou, o número selecionado era ${numberSelected}!`);
+
+  } else if (numberInput !== numberSelected) {
+    addSpokenNumbersScreen(numberInput, false); // adiciona o número na tela
+
+    messageAlert(`Você errou. Tente novamente. Restam ${chances} tentativas`);
+
+    if (chances === 0) {
+      addSpokenNumbersScreen(numberSelected, true); // mostra o número correto na tela
+
+      // desabilita o usuario de continuar jogando sem iniciar um novo setup
+      const buttonInputNumber = document.getElementById("buttonInputNumber");
+      buttonInputNumber.disabled = true;
+
+      messageAlert(`Você errou e suas tentativas acabaram. Mais sorte na próxima.\n\n O número selecionado era ${numberSelected}`);
     }
-  } while (chances > 0);
+  }
+}
+
+function messageAlert(message) {
+  const messageAlertEl = document.querySelector(".messageAlert");
+  messageAlertEl.classList.remove("hide");
+  messageAlertEl.innerText = message;
+}
+
+function addSpokenNumbersScreen(numberInput, hitNumber) {
+  // Se o usuário acertou hitNumber == true
+  const spokenNumbersEl = document.querySelector(".numbers");
+
+  const numberEl = document.createElement("div");
+  numberEl.classList.add("number");
+  numberEl.innerText = numberInput;
+  if (hitNumber) {
+    numberEl.classList.add("right");
+  }
+
+  spokenNumbersEl.appendChild(numberEl);
 }
